c05e9ad4ad7c69df6a0a99484e4a491c8012db24
Blaž Rojc <blaz.rojc.si@gmail.com>
2021-01-02 15:42:20 +0100

    CORS fix.

--- app/main.py
+++ app/main.py
@@ -43,15 +43,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex = r"https:\/\/.*cardmatching.ovh.*",
-    allow_credentials = True,
-    allow_methods = ["*"],
-    allow_headers = ["*"],
-)
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origin_regex = r"http.*localhost.*",
+    allow_origin_regex = r"(http.*localhost.*|https?:\/\/.*cardmatching.ovh.*)",
     allow_credentials = True,
     allow_methods = ["*"],
     allow_headers = ["*"],
